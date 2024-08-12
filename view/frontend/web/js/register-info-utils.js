1bf8e8d2a197a13dea28e0788355e19b16b99aa5
kasch050 <michael.zecher@gmx.at>
2024-08-12 19:51:59 +0200

    Korrektur - anpassung js

--- view/frontend/web/js/register-info-utils.js
+++ view/frontend/web/js/register-info-utils.js
@@ -3,7 +3,7 @@ require(['jquery'], function($) {
         $('select[name="hear_from_us"]').on('change', function() {
             let inputContainer = $('.other-input-container');
             inputContainer.hide();
-            if ( $('option:selected', this).text() === 'Sonstige' ) {
+            if ( $('option:selected', this).text().replace(/\s+/g, '') === 'Sonstige' ) {
                 inputContainer.fadeIn();
             }
         });
